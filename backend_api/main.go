c25bc6aa17ab0cd318accf585fb502d3cd9c21fe
Josh Berryman <jberryman566@gmail.com>
2023-01-13 18:14:58 -0800

    Signup logic

--- backend_api/main.go
+++ backend_api/main.go
@@ -32,43 +32,59 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: homePage")
 }
 
-func signup(w http.ResponseWriter, r *http.Request) {
+func Signup(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: signup")
 	db, ctx, cancel, dberr := connect("mongodb://localhost:27017")
 	if dberr != nil {
+		fmt.Println("Error connecting to DB")
 		panic(dberr)
 	}
+
+	fmt.Println("Request body : ", r)
 	// Parse and decode the request body into a new `Credentials` instance
 	creds := &Credentials{}
 	err := json.NewDecoder(r.Body).Decode(creds)
 	if err != nil {
 		// If there is something wrong with the request body, return a 400 status
+		fmt.Println("Error with request format")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
+	err = nil
 	// Salt and hash the password using the bcrypt algorithm
 	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
-
+	if err != nil {
+		fmt.Println("Error hashing ")
+	}
+	fmt.Println("Password has been hashed")
 	var doc = new(user_doc)
 	doc.Username = creds.Username
 	doc.Hash = string(hashedPassword)
-
+	err = nil
 	collection := db.Database(database).Collection("users")
 	result, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		fmt.Println("Error Inserting to DB")
+		return
+	}
+	fmt.Fprintf(w, "SIGNED YOU UP!")
+	fmt.Println("signup user " + doc.Username + " with hash " + doc.Hash)
+
 	print(result)
 
 	defer cancel()
 	db.Disconnect(ctx)
 }
 
-func signin(w http.ResponseWriter, r *http.Request) {
+func Signin(w http.ResponseWriter, r *http.Request) {
 
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/signup", signup)
-	http.HandleFunc("/singin", signin)
+	http.HandleFunc("/signup", Signup)
+	http.HandleFunc("/singin", Signin)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
