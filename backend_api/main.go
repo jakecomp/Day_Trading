9e3da7b90ece7303b251cdbc99a1e97e1cf7602f
Josh Berryman <jberryman566@gmail.com>
2023-01-14 18:12:28 -0800

    Added sign in verification and added port to docker mongo script

--- backend_api/main.go
+++ backend_api/main.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/crypto/bcrypt"
@@ -34,11 +35,43 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func Signup(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: signup")
-	db, ctx, dberr := connect()
-	if dberr != nil {
-		fmt.Println("Error connecting to DB")
-		panic(dberr)
+	db, ctx := connect()
+
+	// Parse and decode the request body into a new `Credentials` instance
+	creds := &Credentials{}
+	err := json.NewDecoder(r.Body).Decode(creds)
+	if err != nil {
+		// If there is something wrong with the request body, return a 400 status
+		fmt.Println("Error with request format")
+		w.WriteHeader(http.StatusBadRequest)
+		panic(err)
 	}
+
+	// Salt and hash the password using the bcrypt algorithm
+	hashedPassword := hashPassword(creds.Password)
+
+	// Create User Doc for DB
+	var doc = new(user_doc)
+	doc.Username = creds.Username
+	doc.Hash = string(hashedPassword)
+
+	// Save User Doc to MongoDB
+	collection := db.Database(database).Collection("users")
+	result, err := collection.InsertOne(context.TODO(), doc)
+	db.Disconnect(ctx)
+	if err != nil {
+		fmt.Println("Error Inserting to DB: ", err)
+		return
+	}
+
+	fmt.Fprintf(w, "SIGNED YOU UP!")
+	fmt.Println("signup user " + doc.Username + " with hash " + doc.Hash)
+	print(result)
+}
+
+func Signin(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: signin")
+	db, ctx := connect()
 
 	// Parse and decode the request body into a new `Credentials` instance
 	creds := &Credentials{}
@@ -49,41 +82,31 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	err = nil
-	// Salt and hash the password using the bcrypt algorithm
-	// The second argument is the cost of hashing, which we arbitrarily set as 8 (this value can be more or less, depending on the computing power you wish to utilize)
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(creds.Password), 8)
+
+	// Grab Stored Hash and Compare
+	var result user_doc
+	err = db.Database(database).Collection("users").FindOne(ctx, bson.D{{"username", creds.Username}}).Decode(&result)
+	db.Disconnect(ctx)
 	if err != nil {
-		fmt.Println("Error hashing: ", err)
-		return
+		fmt.Println("Error search for record: ", err)
+		panic(err)
 	}
-	fmt.Println("Password has been hashed")
-	var doc = new(user_doc)
-	doc.Username = creds.Username
-	doc.Hash = string(hashedPassword)
-	err = nil
-	collection := db.Database(database).Collection("users")
-	result, err := collection.InsertOne(context.TODO(), doc)
+
+	err = bcrypt.CompareHashAndPassword([]byte(result.Hash), []byte(creds.Password))
 	if err != nil {
-		fmt.Println("Error Inserting to DB: ", err)
-		return
+		fmt.Println("Login or Password is incorrect!")
+		panic(err)
 	}
-	fmt.Fprintf(w, "SIGNED YOU UP!")
-	fmt.Println("signup user " + doc.Username + " with hash " + doc.Hash)
 
-	print(result)
+	fmt.Println("Verified user " + creds.Username)
 
-	defer db.Disconnect(ctx)
-}
-
-func Signin(w http.ResponseWriter, r *http.Request) {
-
+	// Generate JWT and Return It
 }
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
 	http.HandleFunc("/signup", Signup)
-	http.HandleFunc("/singin", Signin)
+	http.HandleFunc("/signin", Signin)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
@@ -91,10 +114,24 @@ func main() {
 	handleRequests()
 }
 
-func connect() (*mongo.Client, context.Context, error) {
+func connect() (*mongo.Client, context.Context) {
 	clientOptions := options.Client()
 	clientOptions.ApplyURI("mongodb://admin:admin@localhost:27017")
 	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 	client, err := mongo.Connect(ctx, clientOptions)
-	return client, ctx, err
+
+	if err != nil {
+		fmt.Println("Error connecting to DB")
+		panic(err)
+	}
+	return client, ctx
+}
+
+func hashPassword(password string) string {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		fmt.Println("Error hashing: ", err)
+		panic(err)
+	}
+	return string(hashedPassword)
 }
