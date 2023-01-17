408dea34fb89b816d953506357f71f7020508481
Josh Berryman <jberryman566@gmail.com>
2023-01-16 16:08:08 -0800

    Added JWT generation and validation logic

--- backend_api/main.go
+++ backend_api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -101,6 +103,13 @@ func Signin(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Verified user " + creds.Username)
 
 	// Generate JWT and Return It
+	token, err := generateJWT(creds.Username)
+	if err != nil {
+		fmt.Println("Error generating JWT: ", err)
+		panic(err)
+	}
+	fmt.Fprintf(w, token)
+	fmt.Println("Token generated: ", string(token))
 }
 
 func handleRequests() {
@@ -134,4 +143,56 @@ func hashPassword(password string) string {
 		panic(err)
 	}
 	return string(hashedPassword)
+}
+
+var sampleSecretKey = []byte("THISISASECRETKEY")
+
+func generateJWT(username string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["authorized"] = true
+	claims["username"] = username
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString(sampleSecretKey)
+
+	if err != nil {
+		fmt.Errorf("Something Went Wrong: %s", err.Error())
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func validateToken(w http.ResponseWriter, r *http.Request) (err error) {
+	if r.Header["Token"] == nil {
+		fmt.Fprintf(w, "can not find token in header")
+		return errors.New("Token error")
+	}
+
+	token, err := jwt.Parse(r.Header["Token"][0], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error in parsing")
+		}
+		return sampleSecretKey, nil
+	})
+
+	if token == nil {
+		fmt.Fprintf(w, "invalid token")
+		return errors.New("Token error")
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		fmt.Fprintf(w, "couldn't parse claims")
+		return errors.New("Token error")
+	}
+
+	exp := claims["exp"].(float64)
+	if int64(exp) < time.Now().Local().Unix() {
+		fmt.Fprintf(w, "token expired")
+		return errors.New("Token error")
+	}
+
+	return nil
 }
