6084e5aa1b45353900b0b19ccab7f4f123272dee
Edra Prasetio <67980359+edraprasetio@users.noreply.github.com>
2023-02-28 11:54:29 -0800

    Dta 13 create contents and cards (#3)
    * DTA-13: Create contents and cards
    
    * DTA-13: Update JSON

---
+++ frontend/src/components/sign_in/text.tsx
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
+
+export const SignInHeader = styled.div`
+    font-size: 48px;
+    color: #000000;
+    line-height: 43.88px;
+`
+
+export const SignInText = styled.div`
+    font-size: 16px;
+    color: #7c80a5;
+    line-height: 19.5px;
+`
+
+export const SignInLink = styled(Link)`
+    font-size: 16px;
+    color: #000000;
+    line-height: 19.5px;
+    &:hover {
+        color: #4555e5;
+    }
+`
+
+export const SignInputPlaceholder = styled.div`
+    font-size: 14px;
+    color: #7c80a5;
+    line-height: 17.07px;
+`
+
+export const SignInputLabel = styled.label`
+    font-size: 16px;
+    color: #000000;
+    line-height: 19.5px;
+`
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,77 @@
+import { HomeBackground, NavbarBackground } from '../components/home/background'
+import { SignBackground } from '../components/sign_in/background'
+import logo from '../assets/logo/Carbs_light.svg'
+import wallet from '../assets/logo/Wallet_duotone_line.svg'
+import download from '../assets/logo/Load_circle.svg'
+import lable from '../assets/logo/lable_duotone.svg'
+import paper from '../assets/logo/Paper_duotone_line.svg'
+import user from '../assets/logo/User_circle.svg'
+import {
+    DataContainer,
+    DataTextContainer,
+    DataValue,
+    UserContainer,
+    UserTextContainer,
+    DataName,
+    StickyContainer,
+    BottomContainer,
+} from '../components/home/containers'
+import { StatusCard, StocksCard, TradesCard } from '../components/home/card'
+import { Header1, Header2 } from '../components/atoms/fonts'
+
+export const Home = () => {
+    return (
+        <div>
+            <StickyContainer>
+                <NavbarBackground>
+                    <UserTextContainer>
+                        <img src={logo} />
+                        <Header1>DTA</Header1>
+                    </UserTextContainer>
+                    <UserContainer>
+                        <UserTextContainer>
+                            <Header2>John Smith</Header2>
+                            <img src={user} />
+                        </UserTextContainer>
+                    </UserContainer>
+                </NavbarBackground>
+            </StickyContainer>
+            <HomeBackground>
+                <StatusCard>
+                    <DataContainer>
+                        <img src={wallet} />
+                        <DataTextContainer>
+                            <DataValue>$5,912</DataValue>
+                            <DataName>Trading Balance</DataName>
+                        </DataTextContainer>
+                    </DataContainer>
+                    <DataContainer>
+                        <img src={download} />
+                        <DataTextContainer>
+                            <DataValue>$8,426</DataValue>
+                            <DataName>Investment</DataName>
+                        </DataTextContainer>
+                    </DataContainer>
+                    <DataContainer>
+                        <img src={lable} />
+                        <DataTextContainer>
+                            <DataValue>185%</DataValue>
+                            <DataName>Rate of Return</DataName>
+                        </DataTextContainer>
+                    </DataContainer>
+                    <DataContainer>
+                        <img src={paper} />
+                        <DataTextContainer>
+                            <DataValue>419</DataValue>
+                            <DataName>Number of Trades</DataName>
+                        </DataTextContainer>
+                    </DataContainer>
+                </StatusCard>
+                <BottomContainer>
+                    <TradesCard></TradesCard>
+                    <StocksCard></StocksCard>
+                </BottomContainer>
+            </HomeBackground>
+        </div>
+    )
+}
