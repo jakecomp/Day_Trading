c5c6b7770880d3b1216f6053db6e03c96e8c5f07
jakecomp <jakobvalen9@gmail.com>
2023-03-01 00:11:41 -0800

    Add quote_server and update compose file

---
+++ quote_server/quote_server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type quote struct {
+	Stock string  `json:"stock"`
+	Price float64 `json:"price"`
+}
+
+func gen_quote(c *gin.Context) {
+
+	var max = 500.0
+	var min = 10.0
+
+	var rand_price = min + rand.Float64()*(max-min)
+
+	var new_quote = []quote{
+
+		{Stock: "s", Price: rand_price},
+	}
+
+	c.IndentedJSON(http.StatusOK, new_quote)
+
+}
+
+func main() {
+
+	fmt.Println("Quote Server runing on Port 8002...")
+	router := gin.Default()
+	router.GET("/", gen_quote)
+
+	router.Run("localhost:8002")
+}
