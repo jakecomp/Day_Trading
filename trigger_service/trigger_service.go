e6b010598870a0a7cf3f590025d658b954c91b1a
Josh Berryman <jberryman566@gmail.com>
2023-03-28 16:46:11 -0700

    Removed unused services and made changes to trigger service

--- trigger_service/trigger_service.go
+++ trigger_service/trigger_service.go
@@ -178,15 +178,15 @@ func dial(url string) (*amqp.Connection, error) {
 
 }
 
-func getQuote() *quote {
-	// Get a quote
-	resp, _ := http.Get("http://10.9.0.6:8002")
-	quote := &quote{}
-	if resp.StatusCode == http.StatusOK {
-		json.NewDecoder(resp.Body).Decode(quote)
-	}
-	return quote
-}
+// func getQuote() *quote {
+// 	// Get a quote
+// 	resp, _ := http.Get("http://10.9.0.6:8002")
+// 	quote := &quote{}
+// 	if resp.StatusCode == http.StatusOK {
+// 		json.NewDecoder(resp.Body).Decode(quote)
+// 	}
+// 	return quote
+// }
 
 func main() {
 	log.SetOutput(ioutil.Discard)
@@ -203,5 +203,117 @@ func main() {
 	userMap = make(map[string]UserTriggers)
 
 	log.Println("RUNNING ON PORT 8004...")
-	handleRequests()
+	clientCode(conn)
+	//handleRequests()
 }
+
+// ======= From Here On This Can Be Used For Implementing The Trigger Service =====
+func setupStockListener(conn *amqp.Connection) (*amqp.Queue, *amqp.Channel, error) {
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	// Setup a queue that we want to subscribe to
+	err = ch.ExchangeDeclare(
+		"stock_prices", // name
+		"fanout",       // type
+		true,           // durable
+		false,          // auto-deleted
+		false,          // internal
+		false,          // no-wait
+		nil,            // arguments
+	)
+	if err != nil {
+		log.Println("Failed to declare an exchange")
+		return nil, nil, err
+	}
+
+	// Create a temperary queue that will let us subscribe without
+	// removing from the main queue for everyone else
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	if err != nil {
+		log.Println("Failed to declare a queue")
+		return nil, nil, err
+	}
+
+	// Bind our temperary queue to the global exchange (subscribe to stock prices)
+	err = ch.QueueBind(
+		q.Name,         // queue name
+		"",             // routing key
+		"stock_prices", // exchange
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Println("Failed to bind a queue")
+		return nil, nil, err
+	}
+	failOnError(err, "Failed to bind a queue")
+	return &q, ch, err
+}
+func clientCode(conn *amqp.Connection) {
+	// Create a channel for recieving stock values
+	q, ch, err := setupStockListener(conn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ch.Close()
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	// Create a channel for requesting stocks
+	neededStocks, err := conn.Channel()
+	neededStocksQ, err := neededStocks.QueueDeclare(
+		"stock_requests", // name
+		false,            // durable
+		false,            // delete when unused
+		false,            // exclusive
+		false,            // no-wait
+		nil,              // arguments
+	)
+	// Example of requesting 10 instances of the stock "S"
+	for i := 0; i < 10; i++ {
+
+		err = neededStocks.Publish(
+			"",                 // name
+			neededStocksQ.Name, // routing key
+			false,              // mandatory
+			false,              // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte("S"),
+			})
+		if err != nil {
+			log.Println(err)
+		}
+	}
+	// Print all the stocks we get back
+	go func() {
+		for d := range msgs {
+			log.Printf(" [x] got %s", d.Body)
+		}
+	}()
+	log.Printf(" [*] Waiting for logs. To exit press CTRL+C")
+
+	var forever chan struct{}
+	<-forever
+}
+
+// func pingStocksQueuer() {
+
+// }
