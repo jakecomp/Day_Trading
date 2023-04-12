103d9d7d45a52e6ffd2ca0a90a584991e013c4ed
Dylan <dylanjkemp@hotmail.com>
2023-04-12 13:39:41 -0700

    fix quote server

--- quote_server/quote_server.go
+++ quote_server/quote_server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
+	"os/signal"
 	"sync"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -72,11 +77,15 @@ func gen_quote_for_all_stocks(c *gin.Context) {
 
 func main() {
 
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	gin.SetMode(gin.ReleaseMode)
 	known_stocks.stocks = make(map[string]quote, 0)
 	known_stocks.stocks["S"] = quote{"S", float64(100)}
 	fmt.Println("Quote Server runing on Port 8002...")
-	router := gin.New()
+	router := gin.Default()
 	router.GET("/", gen_quote)
 	// To get the stock price of R
 	// curl localhost:8002/R
@@ -85,5 +94,37 @@ func main() {
 	// curl localhost:8002/all
 	router.GET("/all", gen_quote_for_all_stocks)
 
-	router.Run("10.9.0.6:8002")
+	//router.Run("10.9.0.6:8002")
+	//http.ListenAndServe("10.9.0.6:8002", router)
+
+	srv := &http.Server{
+		Addr:    ":8002",
+		Handler: router,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
 }
